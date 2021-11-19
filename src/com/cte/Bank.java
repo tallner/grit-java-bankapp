cf268ba4524ada64309aa47ca41a07961abfd952
tallnerc <ctallner@hotmail.com>
2021-11-19 18:22:56 +0100

    In Bank.java changeCustomerName and supportfunction findCustomer implemented

--- src/com/cte/Bank.java
+++ src/com/cte/Bank.java
@@ -13,6 +13,18 @@ public class Bank {
         this.customers = new ArrayList<>();
     }
 
+    //returns the customer index if found, else -1
+    private int findCustomer(long SSN){
+        //returns the customer index if found, else -1
+        int customerIndex=-1;
+        for (int i = 0; i < customers.size(); i++) {
+            if (customers.get(i).getSSN()==SSN){
+                customerIndex = i;
+            }
+        }
+        return customerIndex;
+    }
+
     public List<String> getCustomers(){
         List<String> customerList = new ArrayList<>();
 
@@ -30,51 +42,43 @@ public class Bank {
     }
 
     public boolean addCustomer(String firstName, String lastName, long SSN){
-        boolean customerAlreadyExist=false;
-
-        for (int i = 0; i < customers.size(); i++) {
-            customerAlreadyExist = customers.get(i).getSSN()==SSN;
-        }
-
-        if (!customerAlreadyExist){
-            customers.add(new Customer(firstName,lastName,SSN));
-        }
-
-        return customerAlreadyExist;
+        if (findCustomer(SSN) != -1) customers.add(new Customer(firstName,lastName,SSN));
+        return findCustomer(SSN) != -1;
     }
 
     public List<String> getCustomer(long SSN){
+        //check if customer does not exist
+        if (findCustomer(SSN) == -1) return null;
+
         List<String> customerInfo = new ArrayList<>();
-        for (int i = 0; i < customers.size() ; i++) {
-            if (customers.get(i).getSSN() == SSN){
 
-                customerInfo.add(
-                        customers.get(i).getFirstName()
-                        +" "+
-                        customers.get(i).getLastName());
+        //add customer name
+        customerInfo.add("Customer name: "+customers.get(findCustomer(SSN)).getFirstName()+" "+customers.get(findCustomer(SSN)).getLastName());
 
-                for (int j = 0; j < customers.get(i).getAccounts().size(); j++) {
-                    customerInfo.add(
-                        "Account number: "+
-                        customers.get(i).getAccounts().get(j).getAccountNr()
-                        +", Balance: "+
-                        customers.get(i).getAccounts().get(j).getBalance()
-                        +", Account type: "+
-                        customers.get(i).getAccounts().get(j).getType()
-                    );
-
-                }
-
-            }
-
-        }
-        return customerInfo;
+        //add account information
+        for (int i = 0; i < customers.get(findCustomer(SSN)).getAccounts().size(); i++) {
+            customerInfo.add(
+                "Account number: "+
+                customers.get(findCustomer(SSN)).getAccounts().get(i).getAccountNr()
+                +", Balance: "+
+                customers.get(findCustomer(SSN)).getAccounts().get(i).getBalance()
+                +", Account type: "+
+                customers.get(findCustomer(SSN)).getAccounts().get(i).getType()
+            );
+        } return customerInfo;
     }
-//
-//    public changeCustomerName(String name, long SSN){
-//
-//    }
-//
+
+    public boolean changeCustomerName(String firstName, String lastName, long SSN){
+        //check if customer does not exist
+        if (findCustomer(SSN) == -1) return false;
+
+        //set name if customer found
+        customers.get(findCustomer(SSN)).setFirstName(firstName);
+        customers.get(findCustomer(SSN)).setLastName(lastName);
+
+        return true;
+    }
+
 //    public removeCustomer(long SSN){
 //
 //    }
