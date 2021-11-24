f6c447b83abac588e72d670a9f66b1bf3a85330d
tallnerc <ctallner@hotmail.com>
2021-11-24 11:32:55 +0100

    Mocked data implemented

--- src/com/cte/MockingData.java
+++ src/com/cte/MockingData.java
@@ -13,22 +13,41 @@ public class MockingData {
         var c2 = new Customer("FirstName2", "LastName2", 19990202);
         var c3 = new Customer("FirstName3", "LastName3", 19990303);
 
+
         c1.addAccount("credit",1.4F);
-        c2.addAccount("credit",1.4F);
-        c3.addAccount("credit",1.4F);
+        c1.addAccount("credit",1.45F);
+        c1.addAccount("credit",1.46F);
 
-        List<Customer> customers = new ArrayList<>()
-        {
-            {
-                add(c1);
-                add(c2);
-                add(c3);
-            }
-        };
-        // List<Customer> customers = new ArrayList<>(Arrays.asList(c1,c2,c3));
+        c2.addAccount("credit",1.5F);
 
+
+        c3.addAccount("credit",1.8F);
+
+        List<Customer> customers = new ArrayList<>(Arrays.asList(c1,c2,c3));
 
         Customers.addAll(customers);
 
+    }
+
+    public static Bank initiateMymockMyData(Bank b1){
+        //Creating a mocked datafile
+        MockingData.loadCustomerAndAccounts();
+        var Customer = MockingData.Customers;
+
+        for(var customer : Customer){
+            b1.addCustomer(customer.getFirstName(), customer.getLastName(), customer.getSSN());
+            int amount = 2;
+            for (int i = 0; i < customer.getAccounts().size(); i++) {
+                b1.addAccount(
+                        customer.getSSN(),
+                        customer.getAccounts().get(i).getType(),
+                        customer.getAccounts().get(i).getInterestRate());
+
+                b1.deposit(customer.getSSN(),
+                        customer.getAccounts().get(i).getAccountNr(),
+                        amount = (int) Math.pow(amount,2));
+            }
+        }
+        return b1;
     }
 }
